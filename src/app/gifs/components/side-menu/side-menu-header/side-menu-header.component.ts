016e3e9367407ff3c4bd85d63bb9884e9ebc410b
Luis Eduardo <loistronics@gmail.com>
2025-06-15 17:13:40 -0400

    Frontend GifsApp

---
+++ src/app/gifs/components/side-menu/side-menu-header/side-menu-header.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { environment } from '../../../../../environments/environment.development';
+
+@Component({
+  selector: 'gifs-side-menu-header',
+  imports: [],
+  templateUrl: './side-menu-header.component.html',
+})
+export class SideMenuHeaderComponent {
+
+  envs = environment;
+
+ }
---
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -0,0 +1,27 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MenuOption } from '../../../interfaces/MenuOption.interface';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'gifs-side-menu-options',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './side-menu-options.component.html',
+})
+export class SideMenuOptionsComponent {
+
+  menuOptions : MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      sublabel: 'Gifs Populares',
+      route: '/dashboard/trending',
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      sublabel: 'Buscar gifs',
+      route: '/dashboard/search',
+    },
+  ]
+
+ }
