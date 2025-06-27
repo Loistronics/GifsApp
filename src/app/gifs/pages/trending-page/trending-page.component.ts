dbf21487b176128346e74b6c0f30acb4c873efbd
Luis Eduardo <loistronics@gmail.com>
2025-06-26 22:59:37 -0400

    Se agregan funcionalidades de Scroll

--- src/app/gifs/pages/trending-page/trending-page.component.ts
+++ src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, computed, ElementRef, inject, viewChild } from '@angular/core';
 import { GifListComponent } from "../../components/gif-list/gif-list.component";
 import { GifsService } from '../../services/gifs.service';
 
@@ -13,9 +13,22 @@ export default class TrendingPageComponent {
 
   gifService = inject(GifsService);
 
-  
+  scrollDivRef = viewChild<ElementRef<HTMLDivElement>>('groupDiv');
 
-  
+  onScroll(event : Event){
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+    if( !scrollDiv ) return;
 
+    const scrollTop = scrollDiv.scrollTop;
+    const clientHeight = scrollDiv.clientHeight; //Dimension de la pantalla (Punto de vista)
+    const scrollHeight = scrollDiv.scrollHeight; //Maximo posible del scroll
+
+    const estoyAlFinal = scrollTop + clientHeight + 100 >= scrollHeight;
+    
+    if(estoyAlFinal){
+      this.gifService.loadTrendingGifs();
+    }
+
+  }
 
  }
